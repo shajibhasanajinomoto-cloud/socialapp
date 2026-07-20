86de2297ddaf1ef35419752c40d7f9e86df4717c
shajibhasanajinomoto-cloud <shajibhasanajinomoto@gmail.com>
2026-07-20 16:30:11 +0600

    Delete messageRoutes.js

--- messageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getConversation, getChatList } = require("../controllers/messageController");
-const { protect } = require("../middleware/auth");
-
-router.get("/", protect, getChatList);
-router.get("/:otherUserId", protect, getConversation);
-
-module.exports = router;
